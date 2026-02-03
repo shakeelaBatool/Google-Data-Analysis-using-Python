7d717e023b3056e7333a651493453e3ad0766280
Shakeela Batool <shakeelasadpara@gmail.com>
2026-02-03 20:36:38 +0500

    Fix formatting and improve barplot title

--- google_data_analysis_using_python.py
+++ google_data_analysis_using_python.py
@@ -31,7 +31,7 @@ pytrends.build_payload([keyword], cat=0, timeframe ='today 12-m', geo='', gprop=
 region_data = pytrends.interest_by_region()
 region_data=region_data.sort_values(by= keyword, ascending =False).head(15)
 
-plt.figure(figsize =(18,6))
+plt.figure(figsize = (18,6))
 sns.barplot(x =region_data[keyword], y= region_data.index, palette= "Blues_d")
 plt.title(f"Top Countries searching for '{keyword}')")
 plt.xlabel("Interest")
